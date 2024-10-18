b3c58b2c8096ae7f4340c4c0b524cce563632ae5
zpan <498511842@qq.com>
2024-10-18 20:51:59 +0800

    使用路由组件

--- book/urls.py
+++ book/urls.py
@@ -2,6 +2,13 @@ from django.urls import path, re_path
 
 from book import views
 
+# 路由组件
+from rest_framework import routers
+router = routers.DefaultRouter()
+router.register('book', views.BookView, basename='book')
+router.register('publisher', views.PublisherViewSet, basename='publisher')
+router.register('author', views.AuthorViewSet, basename='author')
+
 urlpatterns = [
     # APIView
     # path('books/', views.BookView.as_view()),
@@ -28,11 +35,17 @@ urlpatterns = [
     # re_path('books/(?P<pk>\d+)/', views.BookView.as_view({'get': 'retrieve', 'put': 'update', 'delete': 'destroy'})),
 
     # ModelViewSet 修改路由分发机制
-    path('books/', views.BookView.as_view({'get': 'list', 'post': 'create'})),
-    re_path('books/(?P<pk>\d+)/', views.BookView.as_view({'get': 'retrieve', 'put': 'update', 'delete': 'destroy'})),
-    path('publisher/', views.PublisherViewSet.as_view({'get': 'list', 'post': 'create'})),
-    re_path('publisher/(?P<pk>\d+)/', views.PublisherViewSet.as_view({'get': 'retrieve', 'put': 'update', 'delete': 'destroy'})),
-    path('author/', views.AuthorViewSet.as_view({'get': 'list', 'post': 'create'})),
-    re_path('author/(?P<pk>\d+)/', views.AuthorViewSet.as_view({'get': 'retrieve', 'put': 'update', 'delete': 'destroy'})),
+    # path('books/', views.BookView.as_view({'get': 'list', 'post': 'create'})),
+    # re_path('books/(?P<pk>\d+)/', views.BookView.as_view({'get': 'retrieve', 'put': 'update', 'delete': 'destroy'})),
+    # path('publisher/', views.PublisherViewSet.as_view({'get': 'list', 'post': 'create'})),
+    # re_path('publisher/(?P<pk>\d+)/', views.PublisherViewSet.as_view({'get': 'retrieve', 'put': 'update', 'delete': 'destroy'})),
+    # path('author/', views.AuthorViewSet.as_view({'get': 'list', 'post': 'create'})),
+    # re_path('author/(?P<pk>\d+)/', views.AuthorViewSet.as_view({'get': 'retrieve', 'put': 'update', 'delete': 'destroy'})),
+
+
+
 
 ]
+
+# 路由组件# 路由工具
+urlpatterns += router.urls
